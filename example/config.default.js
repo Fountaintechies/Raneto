f7fa9e8c2657b68642878a41a8d40ffedbd1721f
amolc <amol.chawathe@fountaintechies.com>
2016-12-04 22:48:32 +0800

    Small updates

--- example/config.default.js
+++ example/config.default.js
@@ -13,7 +13,7 @@ var config = {
   support_email: '',
 
   // Footer Text / Copyright
-  copyright: 'Copyright &copy; '+ new Date().getFullYear() +' - <a href="http://raneto.com">Powered by Raneto</a>',
+  copyright: 'Copyright &copy; '+ new Date().getFullYear() +' - <a href="http://raneto.com">Amol Chawathe</a>',
 
   // Excerpt length (used in search)
   excerpt_length: 400,
---
+++ example/content/80shops/Planning/Requirements.md
@@ -0,0 +1,13 @@
+
+Summary
+We need to make something like flipkart
+
+
+
+
+===Super Admin ==
+
+
+- Business List
+- Order list
+- Payment List
